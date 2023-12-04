1770c85431b255a6a9599721dde30efa2cf44961
Lucas Pinheiro <lucaslogos@gmail.com>
2023-12-04 14:11:19 -0300

    update

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using System.Text.Json;
 using System.Collections.Generic;
 using CursoConsumoAPI.Denpedencias.Modelo;
 
-using (HttpClient client = new HttpClient())
+using (HttpClient client = new())
 {
     string SINTEGRA_API = "https://86b73228-4c2b-40a0-953f-0f91909e228c.mock.pstmn.io/api/v1/execute-api.php?token=ED55ABD3-5343-4046-B3B4-C98E48F7B440&cnpj=56156274000129&plugin=ST";
     try
@@ -14,6 +14,7 @@ using (HttpClient client = new HttpClient())
         var fornecedores = JsonSerializer.Deserialize<Fornecedor>(resposta);
         //Console.WriteLine(resposta);
         Console.WriteLine($"Código do fornecedor:{fornecedores.Codigo}");
+        Console.WriteLine($"Código do fornecedor:{fornecedores.Uf}");
         //Console.WriteLine(DetalhesAPI);
 
 
